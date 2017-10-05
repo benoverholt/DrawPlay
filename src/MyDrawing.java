d4313a9d5e8e6c4d99394d6d680110ee480d1e25
Ben Overholt <overholt@mymail.mines.edu>
2017-10-05 10:34:02 -0600

    First Commit

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -3,20 +3,20 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+
 public class MyDrawing extends JPanel {
 	private Cat cat;
-
-	public MyDrawing() {
+	public MyDrawing() 
+	{
 		// Add critters to the drawing
 		cat = new Cat();
 	}
-
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 50, 50);
 	}
-
+	
 	/**
 	 * @param args
 	 */
@@ -28,7 +28,7 @@ public class MyDrawing extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Set the size (otherwise only title bar displays)
 		frame.setSize(400, 400);
-
+		
 		// Frame will not display until you set visible true
 		frame.setVisible(true);
 	}
